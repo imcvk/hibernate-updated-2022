a78c85473c03e1d66143307686bfb8ad13584ab2
imcvk <chaitanyakulkarni789@gmail.com>
2022-10-16 22:37:37 +0530

    Hibernate - Insert & Reading image using XML and Annotation Config

--- hibernate-image-text-insert-read/src/main/java/xml/entities/Company.java
+++ hibernate-image-text-insert-read/src/main/java/xml/entities/Company.java
@@ -1,14 +1,16 @@
 package xml.entities;
 
-import java.sql.Blob;
+
 
 public class Company {
 	private int companyId;
 	private byte[] image;
+	private char[] resume;
 
-	public Company(int companyId, byte[] image) {
+	public Company(int companyId, byte[] image, char[] resume) {
 		this.companyId = companyId;
 		this.image = image;
+		this.resume = resume;
 	}
 
 	public Company() {
@@ -28,5 +30,20 @@ public class Company {
 
 	public void setImage(byte[] image) {
 		this.image = image;
+	}
+
+	public char[] getResume() {
+		return resume;
+	}
+
+	public void setResume(char[] resume) {
+		this.resume = resume;
+	}
+
+	@Override
+	public String toString() {
+		return "Company{" +
+				"companyId=" + companyId +
+				'}';
 	}
 }
--- hibernate-image-text-insert-read/src/main/java/xml/mainapp/MainApp.java
+++ hibernate-image-text-insert-read/src/main/java/xml/mainapp/MainApp.java
@@ -5,21 +5,36 @@ import org.hibernate.Transaction;
 import xml.entities.Company;
 import xml.util.HibernateUtil;
 
-import java.io.FileInputStream;
+import java.io.*;
 
 public class MainApp {
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 		Session session = HibernateUtil.getXMLSessionFactory().openSession();
 		Transaction transaction = session.beginTransaction();
-		Company company = new Company();
-		FileInputStream fis=new FileInputStream("src/main/java/pic.png");
-		byte[] data=new byte[fis.available()];
-		fis.read (data);
-		company.setImage(data);
-		company.setCompanyId(454);
+		Company company = bootstrapCompany();
+		System.out.println(company);
 		session.persist(company);
 		transaction.commit();
 		session.close();
 	}
 
+	public static Company bootstrapCompany() {
+		Company company = new Company();
+		try {
+			File picFile = new File("hibernate-image-text-insert-read/src/main/resources/pic.png");
+			InputStream inputStream = new FileInputStream(picFile);
+			byte[] pic = new byte[(int) picFile.length()];
+			inputStream.read(pic);
+
+			File resumeFile = new File("hibernate-image-text-insert-read/src/main/resources/test.txt");
+			Reader reader = new FileReader(resumeFile);
+			char[] resume = new char[(int) resumeFile.length()];
+			reader.read(resume);
+			company.setResume(resume);
+			company.setImage(pic);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return company;
+	}
 }
